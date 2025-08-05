9301de63dd2d260e0fd0508a3c78f234ba0d8a03
Azizbek16l <azizbektopilboyev7@gmail.com>
2025-08-05 16:16:14 +0500

    fixed header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,15 @@ function Header() {
   return (
     <header>
         <div className="container">
-
+        <h2 id='logo'>Skilline</h2>  
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Careers</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#" id='login'>Login</a></li>
+            <li><a href="#" id='signup'>Sign up</a></li>
+        </ul>
         </div>
     </header>
   )
