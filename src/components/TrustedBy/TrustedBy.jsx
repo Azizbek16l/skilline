f4652995173d540cb7dc69d9684ffa24bd6180ac
Azizbek16l <azizbektopilboyev7@gmail.com>
2025-08-07 15:55:58 +0500

    fixed

--- src/components/TrustedBy/TrustedBy.jsx
+++ src/components/TrustedBy/TrustedBy.jsx
@@ -4,7 +4,8 @@ function TrustedBy() {
   return (
     <section id='trustedby'>
          <div className="container">
-
+          <h2 id='trusted-title'> Trusted by 5,000+ Companies Worldwide</h2>
+          <img src="https://i.ibb.co/Jjf5gQH6/Group-16.png" id='trusted-img'/>
         </div>
     </section>
   )
