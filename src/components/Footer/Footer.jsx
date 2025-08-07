cd7e8f2fa5a25ce614fe752b95947d24bd02aee7
Azizbek16l <azizbektopilboyev7@gmail.com>
2025-08-07 15:29:44 +0500

    footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,13 +1,39 @@
-import './Footer.css'
+import React from 'react';
+import './Footer.css'; // Agar CSS fayl ajratilgan bo‘lsa
 
-function Footer() {
+const Footer = () => {
   return (
-    <footer>
-         <div className="container">
-
+    <footer className="footer">
+      <div className="container">
+        <div className="footer-top">
+          <div className="logo">
+            <span className="diamond">◈</span>
+            <span className="text-bold">Skill</span>ine
+          </div>
+          <div className="newsletter">
+            <p>Subscribe to get our Newsletter</p>
+            <form>
+              <input type="email" placeholder="Your Email" />
+              <button type="submit" id='Subscribe'> Subscribe</button>
+            </form>
+          </div>
         </div>
-    </footer>
-  )
-}
 
-export default Footer
+        <div className="footer-bottom">
+          <div className="links">
+            <a href="#">Careers</a>
+            <span>|</span>
+            <a href="#">Privacy Policy</a>
+            <span>|</span>
+            <a href="#">Terms & Conditions</a>
+          </div>
+          <div className="copyright">
+            © 2021 Class Technologies Inc.
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/HeroSection/Hero.jsx
+++ src/components/HeroSection/Hero.jsx
@@ -1,10 +1,21 @@
 import './Hero.css'
-
+import { FaPlay } from "react-icons/fa";
 function Hero() {
   return (
     <main>
          <div className="container">
-        
+          <div className='left'>
+            <h1>
+              <span id='span-text'>Studying</span>
+                Online is now much easier
+            </h1>
+            <p>Skilline is an interesting platform that will teach <br/> you in more an interactive way</p>
+            <div className='btns'> <button>Join for free</button>
+            <button><FaPlay /></button></div>
+          </div>
+          <div className='img-box'>
+            <img src="https://i.ibb.co/fV040Zc9/hero.png" alt=""/>
+          </div>
         </div>
     </main>
   )
